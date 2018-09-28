e4e0f1a45db6e6af77c9dc240995220ed4d033cf
zgame <zgamecn@gmail.com>
2018-09-28 10:50:33 +0800

    add
    add

--- Java/AllSort/src/com/zsw/HeapSort.java
+++ Java/AllSort/src/com/zsw/HeapSort.java
@@ -5,34 +5,46 @@ public class HeapSort {
 //    private static int[]a =  {0,1,7,4,2,3,9,6,12,8,5,10};
     public static void Main(){
         System.out.println("-------------堆排序--------------");
-        int[] a = {0,1,7,4,2,3,9,6,12,8,5,10};
+        int[] a = {999,1,7,4,2,3,9,6,12,8,5,10,15};
+        pl(a);
         sort(a);
+
+    }
+
+    private static void pl(int[] a){
+        System.out.println("---------------------------");
         for(int i:a) {
-            System.out.println(i);
+            System.out.printf("%d  ",i);
         }
+        System.out.println();
     }
 
     public static void sort(int[] a){
-        int N= a.length;
-        for(int k=N/2;k>=1;k--){
-            sink(a,k,N);
+        int N= a.length-1;                //要排序的长度
+        for(int k=N/2;k>=1;k--){        // 首先要制造大顶堆
+            sink(a,k,N);                // 遍历一半的数据，k是孩子节点
         }
+        pl(a);
         while (N>1){
-            exch(a,1,N--);
-            sink(a,1,N);
+            exch(a,1,N--);          // 把最大的顶点a[1]跟最后的元素（最小的）进行交换， 这样最大的值就放在最后了，然后将最大值排除
+            sink(a,1,N);            // 用排除了最大值的剩余部分再组成新的大顶堆，循环往复
         }
-
+        pl(a);
     }
 
-    private static void sink(int[]a,int k, int N)	//下沉
+    private static void sink(int[]a,int k, int N)	//下沉成大顶堆
     {
-        while(2*k <= N)
+        while(2*k <= N)     // k是父节点
         {
-            int j = 2*k;
-            if(j<N && less(a,j,j+1)) j++;
-            if(!less(a,k,j))break;
-            exch(a,k,j);
-            k = j;
+            int j = 2*k;                            // j为最后一个孩子节点
+            if(j<N && less(a,j,j+1)){           // 如果右孩子大于左孩子，j变成右孩子
+                j++;
+            }
+            if(less(a,j,k)){           //如果孩子节点a[j]小于父节点a[k], 那么就对了
+                break;
+            }
+            exch(a,k,j);   // 如果孩子节点a[j]大于父节点a[k] ，那么交换位置，孩子节点上浮
+            k = j;         // 父节点变更为孩子节点，开始向下继续沉
         }
     }
 
