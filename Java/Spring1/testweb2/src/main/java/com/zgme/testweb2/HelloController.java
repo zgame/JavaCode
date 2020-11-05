d5f4d1b5a6a7d4ac7a325c1b6054f37af55b8d41
朱世伟 <19665944@qq.com>
2020-11-05 17:24:58 +0800

    add

---
+++ Java/Spring1/testweb2/src/main/java/com/zgme/testweb2/HelloController.java
@@ -0,0 +1,23 @@
+package com.zgme.testweb2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+    // 获取.yml 文件中值
+    @Value("${name}")
+    private String name;
+
+    // 获取 age
+    @Value("${age}")
+    private String age;
+
+
+    @RequestMapping("/hello")   // 路径映射
+    public String hello(){
+        return "Hello Spring Boot!" + name + " : " + age;
+    }
+
+}
---
+++ Java/Spring1/testweb2/src/main/java/com/zgme/testweb2/Testweb2Application.java
@@ -0,0 +1,13 @@
+package com.zgme.testweb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testweb2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testweb2Application.class, args);
+    }
+
+}
