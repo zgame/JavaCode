15b7a12c39dd9d6b5bb30bce12332b55e5a91de3
朱世伟 <19665944@qq.com>
2020-11-06 18:31:15 +0800

    add

---
+++ Java/Spring1/testweb3/src/main/java/com/zgame1/testweb3/Testweb3Application.java
@@ -0,0 +1,13 @@
+package com.zgame1.testweb3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testweb3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testweb3Application.class, args);
+    }
+
+}
---
+++ Java/Spring1/testweb3/src/test/java/com/zgame1/testweb3/Testweb3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgame1.testweb3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testweb3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
